d1317c8a9ca02569b5e41ee92e7a5d8ea3a2b49d
maruchanazalia <maruchanycamaron26.com>
2025-10-24 15:44:26 -0600

    YA MI AVANCE

---
+++ Modulo1/TiposVariables/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class Prueba
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Bienvenido a aprender con la concha de tu madre zandy");
+        Console.WriteLine("que esta harta de todo y me la paso aqui haciendo mmds");
+
+        Console.WriteLine("pruebita");
+    }
+}
